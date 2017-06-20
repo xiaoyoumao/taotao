956d5d50010e0f99d77deb6d8e1b54b4f59a07aa
xiaoyoumao <xiaoyoumao91@163.com>
2017-06-20 13:39:40 +0800

    查询ItemCat商品类别 以树形结构显示

---
+++ taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/ItemCatService.java
@@ -0,0 +1,9 @@
+package com.taotao.service;
+
+import java.util.List;
+
+import com.taotao.pojo.EasyUITreeNode;
+
+public interface ItemCatService {
+	List<EasyUITreeNode> getItemCatList(Long parentId);
+}
---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemCatServiceImpl.java
@@ -0,0 +1,44 @@
+package com.taotao.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.dao.TbItemCatDao;
+import com.taotao.pojo.EasyUITreeNode;
+import com.taotao.pojo.TbItemCat;
+import com.taotao.pojo.TbItemCatQuery;
+import com.taotao.pojo.TbItemCatQuery.Criteria;
+import com.taotao.service.ItemCatService;
+
+@Service
+public class ItemCatServiceImpl implements ItemCatService {
+	@Autowired
+	private TbItemCatDao itemCatDao;
+
+	public List<EasyUITreeNode> getItemCatList(Long parentId) {
+		// 1.根据父节点id查询子节点信息
+		TbItemCatQuery query = new TbItemCatQuery();
+		// 2.设置查询条件
+		Criteria criteria = query.createCriteria();
+		// 3.设置parentId
+		criteria.andParentIdEqualTo(parentId);
+		// 4执行查询获取数据
+		List<TbItemCat> list = itemCatDao.selectByExample(query);
+		// 5.转换成EasyUITreeNode
+
+		List<EasyUITreeNode> nodeList = new ArrayList<>();
+		for (TbItemCat tbItemCat : list) {
+			EasyUITreeNode node = new EasyUITreeNode();
+			node.setId(tbItemCat.getId());
+			node.setText(tbItemCat.getName());
+			// 5.1 如果节点下有子节点‘closed’ 没有子节点为‘open’
+			node.setState(tbItemCat.getIsParent() ? "closed" : "open");
+			nodeList.add(node);
+		}
+		return nodeList;
+	}
+
+}
