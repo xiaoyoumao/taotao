fafde2f8a5e7a13266e07970ac3186f107469998
xiaoyoumao <xiaoyoumao91@163.com>
2017-06-21 13:00:45 +0800

    xiugai

--- taotao-manager/taotao-manager-dao/src/main/java/com/taotao/dao/TbContentCategoryDao.java
+++ taotao-manager/taotao-manager-dao/src/main/java/com/taotao/dao/TbContentCategoryDao.java
@@ -6,25 +6,26 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface TbContentCategoryDao {
-    int countByExample(TbContentCategoryQuery example);
+	int countByExample(TbContentCategoryQuery example);
 
-    int deleteByExample(TbContentCategoryQuery example);
+	int deleteByExample(TbContentCategoryQuery example);
 
-    int deleteByPrimaryKey(Long id);
+	int deleteByPrimaryKey(Long id);
 
-    int insert(TbContentCategory record);
+	int insert(TbContentCategory record);
 
-    int insertSelective(TbContentCategory record);
+	int insertSelective(TbContentCategory record);
 
-    List<TbContentCategory> selectByExample(TbContentCategoryQuery example);
+	List<TbContentCategory> selectByExample(TbContentCategoryQuery example);
 
-    TbContentCategory selectByPrimaryKey(Long id);
+	TbContentCategory selectByPrimaryKey(Long id);
 
-    int updateByExampleSelective(@Param("record") TbContentCategory record, @Param("example") TbContentCategoryQuery example);
+	int updateByExampleSelective(@Param("record") TbContentCategory record,
+			@Param("example") TbContentCategoryQuery example);
 
-    int updateByExample(@Param("record") TbContentCategory record, @Param("example") TbContentCategoryQuery example);
+	int updateByExample(@Param("record") TbContentCategory record, @Param("example") TbContentCategoryQuery example);
 
-    int updateByPrimaryKeySelective(TbContentCategory record);
+	int updateByPrimaryKeySelective(TbContentCategory record);
 
-    int updateByPrimaryKey(TbContentCategory record);
+	int updateByPrimaryKey(TbContentCategory record);
 }
