2061cfeb23ea1d8df0d2c36196a2d5ce01e7d35d
xiaoyoumao <xiaoyoumao91@163.com>
2017-06-21 13:02:07 +0800

    添加common支持

---
+++ taotao-common/src/main/java/com/taotao/common/pojo/EasyUITreeNode.java
@@ -0,0 +1,37 @@
+package com.taotao.common.pojo;
+
+import java.io.Serializable;
+
+public class EasyUITreeNode implements Serializable {
+	// 节点ID
+	private Long id;
+	// 节点名称
+	private String text;
+	// 节点状态 有子节点为 “closed” 没有节点为 “open”
+	private String state;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+}
